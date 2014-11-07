5f35453b3a632d4ff4942fdb5764a86f7aeaccf2
jtrussell <jus.russell@gmail.com>
2014-11-07 09:52:10 -0500

    fix: Don't try to send folders, only files

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ module.exports = function(opts) {
   'use strict';
   opts = opts || {};
 
-  var j = require('path').join;
+  var fs = require('fs')
+    , j = require('path').join;
 
   var resMap = {};
 
   var cleanCacheOnRefresh = opts.cleanCacheOnRefresh || true
     , appBase = opts.appBase = j(process.cwd(), 'app')
     , mockBase = opts.mockBase || '/mock';
+
+  var filesOnly = function(dir, paths, cb) {
+    // Asynchronously filter down a bunch of paths to just files (i.e. no
+    // directories)... we can't stream a directory :p
+    var numToCheck = paths.length
+      , checksMade = 0
+      , files = [];
+
+    if(!numToCheck) { return cb(null, files); }
+
+    var done = function() {
+      checksMade++;
+      if(numToCheck === checksMade) {
+        cb(null, files);
+      }
+    };
+
+    paths.forEach(function(p) {
+      var fullP = j(dir, p);
+      fs.stat(fullP, function(err, stats) {
+        if(!err && stats.isFile()) {
+          files.push(p);
+        }
+        done();
+      });
+    });
+  };
 
   return function(req, res, next) {
     if('/' === req.url && cleanCacheOnRefresh) {
@@ -21,21 +49,23 @@ module.exports = function(opts) {
       return next();
     }
 
-    var fs = require('fs')
-      , j = require('path').join
-      , dir = j(appBase, req.url.replace(/[?#].*/,''));
-    fs.readdir(dir, function(err, files) {
-      if(err || !files.length) { return next(); }
-      files.sort();
-      var f, fIx;
-      if(resMap[dir]) {
-        fIx = files.indexOf(resMap[dir]);
-        f = files[(fIx + 1) % files.length];
-      } else {
-        f = files[0];
-      }
-      resMap[dir] = f;
-      fs.createReadStream(j(dir, f)).pipe(res);
+    var dir = j(appBase, req.url.replace(/[?#].*/,''));
+
+    fs.readdir(dir, function(err, paths) {
+      if(err || !paths.length) { return next(); }
+      filesOnly(dir, paths, function(error, files) {
+        if(err || !files.length) { return next(); }
+        files.sort();
+        var f, fIx;
+        if(resMap[dir]) {
+          fIx = files.indexOf(resMap[dir]);
+          f = files[(fIx + 1) % files.length];
+        } else {
+          f = files[0];
+        }
+        resMap[dir] = f;
+        fs.createReadStream(j(dir, f)).pipe(res);
+      });
     });
   };
 };
